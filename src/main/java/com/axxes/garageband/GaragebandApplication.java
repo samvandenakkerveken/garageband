2ca7ddec9c5dfbbbdff6537fc3d99e3188ebb385
svandenakkerveken <sam.vandenakkerveken@axxes.com>
2017-09-06 10:29:10 +0200

    setup

---
+++ src/main/java/com/axxes/garageband/GaragebandApplication.java
@@ -0,0 +1,13 @@
+package com.axxes.garageband;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaragebandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaragebandApplication.class, args);
+
+	}
+}
---
+++ src/test/java/com/axxes/garageband/instruments/InstrumentTest.java
@@ -0,0 +1,25 @@
+package com.axxes.garageband.instruments;
+
+import kuusisto.tinysound.Sound;
+import kuusisto.tinysound.TinySound;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InstrumentTest {
+
+    @Before
+    public void setup(){
+        TinySound.init();
+    }
+
+    @Test
+    public void testPlaySound(){
+        Boolean played = false;
+        Sound sound = TinySound.loadSound("Kick-01.wav");
+        sound.play();
+        played = true;
+        assertThat(played).isTrue();
+    }
+}
