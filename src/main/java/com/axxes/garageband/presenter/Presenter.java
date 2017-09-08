f66761cce00b60dafee31652daee709171bd62e3
Alexander <alexvanravestyn@gmail.com>
2017-09-08 08:56:30 +0200

    Timeline refactor

--- src/main/java/com/axxes/garageband/presenter/Presenter.java
+++ src/main/java/com/axxes/garageband/presenter/Presenter.java
@@ -36,45 +36,20 @@ public class Presenter {
     @FXML
     AnchorPane imageCymbal;
 
-    private Drumloop drumloop = new Drumloop();
-
-    public void startLoop(int bpm) {
-
-
-        int timeBetweenBeats = 60000 / bpm;
-
-        Timeline timeline = new Timeline(new KeyFrame(
-                Duration.millis(timeBetweenBeats),
-                ae -> {
-                    Logger.getLogger(Presenter.class).info("Drumloop step.");
-                    this.drumloop.step();
-                    // UI handling
-                }));
-        timeline.setCycleCount(Animation.INDEFINITE);
-        timeline.play();
-    }
+    Timeline loopTimeline;
 
     public void startLoop(int bpm, Drumloop drumloop) {
         int timeBetweenBeats = 60000 / bpm;
 
-        Timeline timeline = new Timeline(new KeyFrame(
+        this.loopTimeline = new Timeline(new KeyFrame(
                 Duration.millis(timeBetweenBeats),
                 ae -> {
                     Logger.getLogger(Presenter.class).info("Drumloop step.");
                     drumloop.step();
                     // UI handling
                 }));
-        timeline.setCycleCount(Animation.INDEFINITE);
-        timeline.play();
-
-
-        /*Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                drumloop.step();
-            }
-        }, 0, timeBetweenBeats);*/
+        loopTimeline.setCycleCount(Animation.INDEFINITE);
+        loopTimeline.play();
     }
 
     @FXML
